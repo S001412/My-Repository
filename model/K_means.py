03b1731bfe09706938aa60150b961829758e58b7
cxl <alex@cxldeMBP.lan>
2018-12-07 17:24:36 +0800

    keras-vae

---
+++ model/K_means.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.cluster import KMeans, hierarchical
+# import matplotlib.pyplot as plt
+from sklearn import metrics
+import numpy as np
+# from mysql_data_precessing import test_vector
+from user_cf import cosine_similarity
+from operator import itemgetter
+from data_preprocessing import label_data, comment_data_matrix, label_comment_matrix
+from keras.models import load_model
+
+# km = KMeans(n_clusters=10, max_iter=600, n_init=50)
+# y_pred = km.fit_predict(test_vector)
+# c = metrics.calinski_harabaz_score(test_vector, y_pred)
+# # plt.scatter(X[:, 0], X[:, 1], c=y_pred)
+# a = metrics.calinski_harabaz_score(X, y_pred)
+# label_pred = km.labels_ #获取聚类标签
+# centroids = km.cluster_centers_ #获取聚类中心
+# inertia = km.inertia_ # 获取聚类准则的总和
+# # plt.show()
+# bh = Birch(n_clusters=10)
+# y_p2 = bh.fit_predict(test_vector)
+# b = metrics.calinski_harabaz_score(test_vector, y_p2)
+
+
+# 建立相似度矩阵，输入一个字典，输出字典中元素两两之间的相似度,由高到低排序
+def cosine_sim_matrix(set1, matrix):
+    sim_dict = {}
+    for i in set1:
+        sim_dict[i] = {}
+        for j in set1:
+            if i == j:
+                continue
+            else:
+                sim_dict[i][j] = cosine_similarity(matrix[i, :], matrix[j, :])
+        sim_dict[i] = sorted(sim_dict[i].items(), key=itemgetter(1), reverse=True)
+    return sim_dict
+
+
+def cluster(test_vector):
+    hi = hierarchical.AgglomerativeClustering(n_clusters=10)
+    y_pred = hi.fit_predict(test_vector)
+    a = metrics.calinski_harabaz_score(test_vector, y_pred)
+    # 建立类别字典
+    labels = set(y_pred)
+    class_dict = {}
+    for label in labels:
+        class_dict[label] = set()
+    # 将同一类别对应的编号放在同一个键中
+    for y_index in range(len(y_pred)):
+        for label in class_dict:
+            if y_pred[y_index] == label:
+                class_dict[label].add(y_index)
+    return class_dict
+
+
+def distEclud(vecA, vecB):
+    '''
+    输入：向量A和B
+    输出：A和B间的欧式距离
+    '''
+    return np.sqrt(sum(np.power(vecA - vecB, 2)))
+
+
+def newCent(L, label_list):
+    '''
+    输入：有标签数据集L
+    输出：根据L确定初始聚类中心
+    '''
+    centroids = []
+    label_list = np.unique(label_list)
+    for i in label_list:
+        # L_i = L[(L[:,-1]) == i]
+        L_i = L[i: i*10]
+        cent_i = np.mean(L_i, 0)
+        centroids.append(cent_i)
+    return np.array(centroids)
+
+
+def semi_kMeans(L, U, label_list, distMeas=distEclud, initial_centriod=newCent):
+    '''
+    输入：有标签数据集L（最后一列为类别标签）、无标签数据集U（无类别标签）
+    输出：聚类结果
+    '''
+    # 合并L和U
+    dataSet = np.vstack((L, U))
+    label_list = np.unique(label_list)
+    # L中类别个数
+    k = len(label_list)
+    m = np.shape(dataSet)[0]
+    # 初始化样本的分配
+    clusterAssment = np.zeros(m)
+    # 确定初始聚类中心
+    centroids = initial_centriod(L, label_list)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        # 将每个样本分配给最近的聚类中心
+        for i in range(m):
+            minDist = np.inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j, :], dataSet[i, :])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i] != minIndex: clusterChanged = True
+            clusterAssment[i] = minIndex
+    return clusterAssment
+
+
+if __name__ == '__main__':
+    label_ = label_data[:, 0]
+    encoder = load_model('encoder_label.h5')
+    x_train_encoded = encoder.predict(comment_data_matrix)
+    x_test_encoded = encoder.predict(label_comment_matrix)
+    a = semi_kMeans(x_test_encoded, x_train_encoded, label_, distMeas=distEclud, initial_centriod=newCent)
+    a = a[100:]
+    b = metrics.calinski_harabaz_score(x_train_encoded, a)
+    # cluster_dict = cluster()
+    # # 计算某个类别中元素之间的相似度
+    # sim_matrix = cosine_sim_matrix(cluster_dict[0], test_vector)
---
+++ model/data_preprocessing.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import copy
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import preprocessing
+from jieba import analyse
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from gensim.models import KeyedVectors
+from sklearn.model_selection import train_test_split
+
+
+def label_onehot(array, vocab_size, length):
+    vocab_size = vocab_size
+    array_onehot_encoder = [one_hot(d, vocab_size) for d in array]
+    # 扩展至同一长度
+    max_length = length
+    onehot_result_array = pad_sequences(array_onehot_encoder, maxlen=max_length, padding='post')
+    onehot_result_array = preprocessing.normalize(onehot_result_array)
+    return onehot_result_array
+
+
+# 输入数值数据，使用sklearn返回数据的归一化结果
+def min_max_scale(array, row):
+    array = preprocessing.minmax_scale(array)
+    array = array.reshape(row, 1)
+    return array
+
+
+# 输入带汉字的标签，返回onehot结果
+def onehot_encoder(data):
+    # integer encode, 标签转化为onehot
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(data)
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    onehot_result = onehot_encoded
+    return onehot_result
+
+
+# 将分词后的结果转变为词向量，输入 array，输出array
+def word_vectorization(word_model, array, row_array, limit):
+    # 用来存储单个词语向量化后的结果，长度由生成词模型时的设置解果决定
+    size = 100
+    res_array = np.zeros(size * limit)
+    for row_number in range(row_array):
+        if len(array[row_number]) == 1:
+            res_array = np.vstack((res_array, np.zeros(size * limit)))
+            continue
+        else:
+            try:
+                split_list = array[row_number].split(' ')
+                tem_array = word_model[split_list[0]]
+            except KeyError:
+                tem_array = np.zeros(size)
+        for index in range(1, limit):
+            if index < len(split_list):
+                try:
+                    word_vec = word_model[split_list[index]]
+                    tem_array = np.concatenate((tem_array, word_vec))
+                except KeyError:
+                    tem_array = np.concatenate((tem_array, np.zeros(size)))
+            else:
+                tem_array = np.concatenate((tem_array, np.zeros(size)))
+        res_array = np.vstack((res_array, tem_array))
+    res_array = np.delete(res_array, 0, axis=0)
+    return res_array
+
+
+user_data = pd.read_excel('评论信息+用户信息.xlsx')
+# user_data = user_data[['有用数', '感谢次数', '总分', '评论数', '读者数', '景点名称', '评分', '标题', '内容']]
+user_data = user_data[['标题', '内容']]
+user_data = pd.DataFrame(user_data).fillna({'标题': '0', '内容': '0'})
+user_data = np.array(user_data)
+row_user_data = user_data.shape[0]
+column_user_data = user_data.shape[1]
+
+label_data = pd.read_excel('帖子标签.xlsx')
+label_data = np.array(label_data)
+row_label_data = label_data.shape[0]
+
+# user_data_type = user_data[:, 5]
+# input_vec = onehot_encoder(user_data_type)
+
+# user_rating_score = min_max_scale(user_data[:, 6], row_user_data)
+# input_vec = user_rating_score
+
+tfidf = analyse.extract_tags
+# comment_data = copy.deepcopy(user_data[:, 0])
+comment_data = user_data[:, 0]
+for i in range(row_user_data):
+    if type(user_data[i, 0]) != str:
+        user_data[i, 0] = '0'
+    title_list = tfidf(user_data[i, 0])
+    comment_list = tfidf(user_data[i, 1])
+    comment_data[i] = ' '.join(title_list)
+    comment_data[i] = ' '.join(comment_list)
+
+label_comment = label_data[:, 1]
+for l in range(row_label_data):
+    label_comment_list = tfidf(label_data[l, 1])
+    label_comment[l] = ' '.join(label_comment_list)
+
+# 读取模型，构建词向量矩阵
+word_model = KeyedVectors.load_word2vec_format("comment.model.bin", binary=True)
+comment_data_matrix = word_vectorization(word_model, comment_data, row_user_data, 10)
+label_comment_matrix = word_vectorization(word_model, label_comment, row_label_data, 10)
+
+# label_comment_matrix = np.hstack((label_comment_array, np.zeros((row_label_data, 1))))
+#
+# for i in range(row_label_data):
+#     label_comment_matrix[i, -1] = label_data[i, 0]
+
+# x_train, x_test = train_test_split(comment_data_matrix, test_size=0.2)
